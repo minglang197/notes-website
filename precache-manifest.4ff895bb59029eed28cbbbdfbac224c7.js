43cce08b7966d1b048cb479dc706276e311c6ed2
lucidity <jiminglang@foxmail.com>
2021-01-20 15:39:16 +0800

    deploy

---
+++ precache-manifest.4ff895bb59029eed28cbbbdfbac224c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b74e077e77eb712ddf",
+    "url": "/notes-website/css/app.44d647c3.css"
+  },
+  {
+    "revision": "a5cb11b9de64bad130b5c9e3b5652320",
+    "url": "/notes-website/img/logo.a5cb11b9.jpg"
+  },
+  {
+    "revision": "bee713f0fd97ca80b64c833cec86019b",
+    "url": "/notes-website/index.html"
+  },
+  {
+    "revision": "51b74e077e77eb712ddf",
+    "url": "/notes-website/js/app.0cbd962c.js"
+  },
+  {
+    "revision": "e77b2395381b8d4ac718",
+    "url": "/notes-website/js/chunk-vendors.90e84342.js"
+  },
+  {
+    "revision": "2d4910ccb8d88691a5d1b3156dfe163c",
+    "url": "/notes-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/notes-website/robots.txt"
+  }
+]);
